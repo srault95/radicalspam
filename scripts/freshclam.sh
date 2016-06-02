a8fd43f1acb2332a6782a178443bad887853f2c8
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-06-02 19:46:02 +0200

    fixe load unofficial sigs

--- scripts/freshclam.sh
+++ scripts/freshclam.sh
@@ -2,6 +2,6 @@
 
 [ -e /var/lib/clamav/main.cvd ] || freshclam -c 1
 
-[ -e /var/lib/clamav/jurlbl.ndb ] || /usr/sbin/clamav-unofficial-sigs
+[ -e /var/lib/clamav/jurlbl.ndb ] || gosu clamav /usr/sbin/clamav-unofficial-sigs
 
 exec freshclam -d --quiet -c 1 --enable-stats
