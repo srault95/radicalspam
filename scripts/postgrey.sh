3a481d2a11b4cfe4e01dc1add923589171409fdd
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-06-12 16:55:04 +0200

    add verbose

--- scripts/postgrey.sh
+++ scripts/postgrey.sh
@@ -4,4 +4,4 @@ mkdir -p /var/lib/postgrey
 
 chown postgrey.postgrey /var/lib/postgrey
 
-exec /usr/sbin/postgrey --inet=${GREYLIST_HOST} --delay=60 --hostname=${MY_HOSTNAME} --retry-window=5 --auto-whitelist-clients=2
+exec /usr/sbin/postgrey -v --inet=${GREYLIST_HOST} --delay=60 --hostname=${MY_HOSTNAME} --retry-window=5 --auto-whitelist-clients=2
